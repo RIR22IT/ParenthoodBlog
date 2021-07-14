01e674cff042d792a7dfb57dc9c76a891989cc43
Thisara <thisaradilshanbens99@gmail.com>
2021-07-14 23:14:40 +0530

    google reCAPTCHA added

--- auth/login/index.php
+++ auth/login/index.php
@@ -1,19 +1,24 @@
+<?php
+    $siteKey = '6LcDrpYbAAAAAP31-saCAGyHntGEuDjzz0oBEYyG';
+    $secretKey = '6LcDrpYbAAAAANElfTdcQ1MwerEatPQIlJV6NgbV';
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
     <title>LOGIN</title>
-    <link rel="stylesheet" type="text/css" href="../../assets/css/loginstyle.css">
-    <link rel="stylesheet" type="text/css" href="../../assets/css/signup.css">
+    <!-- <link rel="stylesheet" type="text/css" href="../../assets/css/loginstyle.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="../../assets/css/signup.css"> -->
     <link rel="stylesheet" type="text/css" href="../../assets/css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a81368914c.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Google reCAPTCHA -->
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> -->
 </head>
 
 <body>
@@ -27,7 +32,7 @@
                 <img src="../../assets/images/user.svg">
                 <h2 class="title">LOGIN</h2>
                 <?php if (isset($_GET['error'])) { ?>
-                    <p class="error"><?php echo $_GET['error']; ?></p>
+                <p class="error"><?php echo $_GET['error']; ?></p>
                 <?php } ?>
 
                 <div class="input-div one">
@@ -49,10 +54,10 @@
                         <input type="password" name="password" class="input"><br>
                     </div>
                 </div>
-
+                <div class="g-recaptcha" data-sitekey = "<?php echo $siteKey ?>"></div>
                 <input type="submit" class="btn" value="Login">
 
-                <a class="txt-brand" href="../signup/signup.php">Sign Up</a>
+                <a class="txt-brand" href="../signup/signup.php">Need an account?</a>
                 <a class="txt-brand" href="../forgot/forget-password.php">Forgot Password</a>
 
             </form>
--- auth/login/login.php
+++ auth/login/login.php
@@ -2,6 +2,9 @@
 session_start(); 
 include "../../database/connection.php";
 
+$siteKey = '6LcDrpYbAAAAAP31-saCAGyHntGEuDjzz0oBEYyG';
+$secretKey = '6LcDrpYbAAAAANElfTdcQ1MwerEatPQIlJV6NgbV';
+
 if (isset($_POST['email']) && isset($_POST['password'])) {
 
 	function validate($data){
@@ -13,38 +16,57 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
 
 	$email = validate($_POST['email']);
 	$pass = validate($_POST['password']);
-
-	if (empty($email)) {
-		header("Location: index.php?error=Email is required");
-	    exit();
-	}else if(empty($pass)){
-        header("Location: index.php?error=Password is required");
 	
-	    exit();
-	}else{
-
-		// hashing the password
-        $pass = md5($pass);
-
-		$sql = "SELECT * FROM users WHERE email='$email' AND password='$pass'";
-
-		$result = mysqli_query($db, $sql);
-		if (mysqli_num_rows($result) === 1) {
-			$row = mysqli_fetch_assoc($result);
-            if ($row['email'] === $email && $row['password'] === $pass) {
-            	$_SESSION['email'] = $row['email'];
-            	$_SESSION['id'] = $row['id'];
-            	header("Location: home.php");
-		        exit();
-            }else{
-				header("Location: index.php?error=Incorrect email or password");
-		        exit();
+	if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])){
+		//Verify the  reCAPTCHA response
+		$verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secretKey.'&response='.$_POST['g-recaptcha-response']);
+		
+		//Decode json data
+		$responseData = json_decode($verifyResponse);
+		
+		if($responseData->success){
+			if (empty($email)) {
+				header("Location: index.php?error=Email is required");
+				exit();
+			}else if(empty($pass)){
+				header("Location: index.php?error=Password is required");
+				exit();
+			}else{
+		
+				// hashing the password
+				$pass = md5($pass);
+		
+				$sql = "SELECT * FROM users WHERE email='$email' AND password='$pass'";
+		
+				$result = mysqli_query($db, $sql);
+				if (mysqli_num_rows($result) === 1) {
+					$row = mysqli_fetch_assoc($result);
+					if ($row['email'] === $email && $row['password'] === $pass) {
+						$_SESSION['email'] = $row['email'];
+						$_SESSION['id'] = $row['id'];
+						header("Location: home.php");
+						exit();
+					}else{
+						header("Location: index.php?error=Incorrect email or password");
+						exit();
+					}
+				}else{
+					header("Location: index.php?error=Incorrect email or password");
+					exit();
+				}
 			}
 		}else{
-			header("Location: index.php?error=Incorrect email or password");
-	        exit();
+			header("Location: index.php?error=Robot verification failed, please try again.");
+			exit();
 		}
+		
+
+	}else{
+		header("Location: index.php?error=Please check on the reCAPTCHA box");
+		exit();
 	}
+
+	
 	
 }else{
 	header("Location: index.php");
