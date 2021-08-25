297497363b4399797245f24cec7c8428919117bc
thisaraRIR <thisara.rir@gmail.com>
2021-08-25 13:28:30 +0530

    home advertisment image added

--- ParenthoodWeb/admin/pageWrapper.php
+++ ParenthoodWeb/admin/pageWrapper.php
@@ -1,45 +1,45 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="adminPanel.php">
-                <div class="sidebar-brand-icon">
-                    <i class="fas fa-users-cog"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">ADMIN</div>
-            </a>
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="adminPanel.php">
+        <div class="sidebar-brand-icon">
+            <i class="fas fa-users-cog"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">ADMIN</div>
+    </a>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
 
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
-                <a class="nav-link" href="dashboard.php">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>ADMIN PANEL</span></a>
-            </li>
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link" href="dashboard.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>ADMIN PANEL</span></a>
+    </li>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+    <!-- Divider -->
+    <hr class="sidebar-divider">
 
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="footerImageUpload.php">
-                    <i class="fas fa-plus-square"></i>
-                    <span>Add Footer Images</span>
-                </a>
-            </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="footerImageUpload.php">
+            <i class="fas fa-plus-square"></i>
+            <span>Add Footer Images</span>
+        </a>
+    </li>
 
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="homeVideo.php">
-                    <i class="fas fa-plus-square"></i>
-                    <span>Add Home Video</span>
-                </a>
-            </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="homeVideo.php">
+            <i class="fas fa-plus-square"></i>
+            <span>Add Home Video</span>
+        </a>
+    </li>
 
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="homeAd.php">
-                    <i class="fas fa-plus-square"></i>
-                    <span>Add Home Ad</span>
-                </a>
-            </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="homeAd.php">
+            <i class="fas fa-plus-square"></i>
+            <span>Add Home Ad</span>
+        </a>
+    </li>
 
-        </ul>
+</ul>
